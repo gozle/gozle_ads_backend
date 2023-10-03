ccd444a33895d010144de9fc1aa7d8ddf36e21f3
Rowsen <bayramovw0@gmail.com>
2023-10-03 12:46:10 +0500

    added django-debug_toolbar

--- gozle_ads/urls.py
+++ gozle_ads/urls.py
@@ -24,3 +24,6 @@ urlpatterns = [
 
     path("api/v1/", include("ads.urls"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += [path("__debug__", include("debug_toolbar.urls"))]
