a8380dfa54278ee2b4266730a74ea05f6a01d6a8
Rowsen <bayramovw0@gmail.com>
2023-11-18 00:40:23 +0500

    fixing problem with dotenv load

--- gozle_ads/settings/__init__.py
+++ gozle_ads/settings/__init__.py
@@ -4,11 +4,11 @@ from django.conf import settings
 
 from dotenv import load_dotenv
 
-from .base import BASE_DIR
-
 root_urlconf = settings.ROOT_URLCONF
 
 # dotenv config
+
+from .base import BASE_DIR
 dotenv_path = f"{BASE_DIR}/dist.env"
 load_dotenv(dotenv_path)
 
