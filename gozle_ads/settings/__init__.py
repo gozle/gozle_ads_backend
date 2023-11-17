d0e09b78fe6f6287946f3cf57d3c83a3853e5ed0
Rowsen <bayramovw0@gmail.com>
2023-11-18 00:34:04 +0500

    added root urlconf

--- gozle_ads/settings/__init__.py
+++ gozle_ads/settings/__init__.py
@@ -1,7 +1,12 @@
 import os
+
+from django.conf import settings
+
 from dotenv import load_dotenv
 
 from .base import BASE_DIR
+
+root_urlconf = settings.ROOT_URLCONF
 
 # dotenv config
 dotenv_path = f"{BASE_DIR}/dist.env"
