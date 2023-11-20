444504d14e665bdf14358ab2c7ea5eb705325b5e
Rowsen <bayramovw0@gmail.com>
2023-11-20 10:14:11 +0500

    created model of city

--- locations/models.py
+++ locations/models.py
@@ -4,7 +4,17 @@ from django.utils.translation import gettext_lazy as _
 
 class Province(models.Model):
     name = models.CharField(max_length=10)
-    code_name = models.CharField(max_length=3)
+    code_name = models.CharField(max_length=3, unique=True)
 
     def __str__(self):
         return f"{self.code_name} | {self.name}"
+
+
+class City(models.Model):
+    name = models.CharField(max_length=15)
+    code_name = models.CharField(max_length=3, unique=True)
+    province = models.ForeignKey(
+        Province,
+        on_delete=models.CASCADE,
+        related_name="cities"
+    )
